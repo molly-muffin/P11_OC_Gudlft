68946d60250ac87c5740af66286f05d5235d6939
molly-muffin <laureendademeule@gmail.com>
2023-06-11 17:46:47 +0200

    points update reflected

---
+++ Python_Testing-master/README.md
@@ -0,0 +1,13 @@
+# gudlift-registration
+
+## bug-book_more_than_available
+
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/error1.JPG)  
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/error2.JPG)
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/error3.JPG)
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/error4.JPG)
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/error5.JPG)
+
+## correction bug-book_more_than_available
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/correction1.JPG)
+![Alt text](https://github.com/molly-muffin/P11_OC_Gudlft/blob/2.bug-book_more_than_available/Python_Testing-master/images/correction2.JPG)
--- Python_Testing-master/server.py
+++ Python_Testing-master/server.py
@@ -20,6 +20,7 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route('/')
 def index(error=None):
     return render_template('index.html', error=error)
@@ -34,12 +35,16 @@ def showSummary():
     return render_template('welcome.html',club=one_club,competitions=competitions)
 
 
+
+
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
+    global maxPlaces
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    maxPlaces = min(12, int(foundClub["points"]), int(foundCompetition['numberOfPlaces']))
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html',club=foundClub,competition=foundCompetition, maxPlaces=maxPlaces)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -50,10 +55,14 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+    clubPoints = int(club["points"])
+    if clubPoints >= placesRequired:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        club['points'] = int(club['points'])-placesRequired
+        flash('Great-booking complete !')
+    else:
+        flash('Sorry, you do not have the number of available places required to book this number of places.')
     return render_template('welcome.html', club=club, competitions=competitions)
-
 
 # TODO: Add route for points display
 
